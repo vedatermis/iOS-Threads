3646a4f02e39843630a9f11ee738bf754ae32e6f
Vedat ERMİŞ <vedatermis@hotmail.com>
2023-08-25 10:07:49 +0300

    Initial Commit

---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by Vedat ERMIS on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
